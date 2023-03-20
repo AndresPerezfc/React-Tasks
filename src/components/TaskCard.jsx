fba3c9b2893b6f9f1d24f71da1a450267a2c958d
andresperezfc <felipe3apc@gmail.com>
2023-03-20 11:35:20 -0500

    Crear contexto

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,11 +1,16 @@
-
+import {useContext} from 'react'
+import { TaskContext } from '../context/TaskContent'
 
 function TaskCard({task, deleteTask}){
-
+    
+    const valor = useContext(TaskContext);
+    console.log(valor)
     function mostrarAlerta(){
         alert(task.id)
+        
     }
     return (
+        
         <div>
                 <h2>{task.title}</h2>
                 <p>{task.description}</p>
---
+++ src/context/TaskContent.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { createContext } from 'react'
+
+export const TaskContext = createContext();
+
+ export function TaskContextProvider(props) {
+     let x = 20;
+  return (
+      <TaskContext.Provider value={x}>
+          {props.children}
+      </TaskContext.Provider>
+
+  )
+}
+
+
